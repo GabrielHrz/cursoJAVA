792e886102c781850b9ec15b781f47ace4966ab9
GabrielHrz <gabrielalexanderhr@gmail.com>
2024-06-26 14:51:13 +0100

    Tipos primitivos.

---
+++ src/JavaBasico/TiposDeVariables.java
@@ -0,0 +1,23 @@
+package JavaBasico;
+
+public class TiposDeVariables {
+    public static void main(String[] args) {
+
+        /*Tipos Primitivos
+        * int
+        * long
+        * char
+        * float
+        * double
+        * boolean
+        /*
+    /*Objetos
+
+        * String
+        *
+        * StringBuilder
+        *
+        *
+        *  */
+    }
+}
