ded70215839f88cb5eebd57d3ffdfd9873cf0ced
GabrielHrz <gabrielalexanderhr@gmail.com>
2024-06-26 15:32:27 +0100

    Palabras Reservadas en java.

--- src/JavaBasico/TiposDeVariables.java
+++ src/JavaBasico/TiposDeVariables.java
@@ -10,14 +10,19 @@ public class TiposDeVariables {
         * float
         * double
         * boolean
-        /*
+        */
+
+
     /*Objetos
 
         * String
-        *
+        * Integer
         * StringBuilder
         *
         *
         *  */
+        //Declarar varias variables del mismo tipo a la vez
+        int a = 5, b = 10;
+        short c = 12, e = 2;
     }
 }
